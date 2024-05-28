d78b1e0c7cf1fa3d0af9a43ad9d3fcff80215880
cosmos-explorer <lian.yang.work@gmail.com>
2024-05-28 09:50:09 +0200

    Create 20240528053641_create reddit_submission table.sql

---
+++ supabase/migrations/20240528053641_create reddit_submission table.sql
@@ -0,0 +1,16 @@
+/**
+* USERS
+* Note: This table contains user data. Users should only be able to view and update their own data.
+*/
+create table reddit_submission (
+  -- UUID from auth.users
+  id uuid default uuid_generate_v4() primary key,
+  reddit_id text,
+  title text,
+  text text,
+  url text,
+  subreddit text,
+  created_at timestamptz default now()
+);
+alter table users enable row level security;
+create policy "Allow public read-only access." on reddit_submission for select using (true);
