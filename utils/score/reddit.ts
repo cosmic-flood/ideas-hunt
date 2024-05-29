e83979e89338fb35f3bbf9bd4d6af932bca509ed
cosmos-explorer <lian.yang.work@gmail.com>
2024-05-29 08:59:32 +0200

    added search subreddits method

--- utils/score/reddit.ts
+++ utils/score/reddit.ts
@@ -43,6 +43,26 @@ export class RedditClient {
     this.accessToken = authData.access_token;
   }
 
+  static async searchSubreddits(
+    query: string,
+    limit: number = 10,
+  ): Promise<string[]> {
+    const response = await fetch(
+      `https://www.reddit.com/api/subreddit_autocomplete_v2.json?query=${query}&include_over_18=0&include_profiles=0&limit=${limit}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to search subreddit: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const subreddits = data.data.children.map(
+      (subreddit: any) => subreddit.data.display_name_prefixed,
+    );
+
+    return subreddits;
+  }
+
   async getNew(
     subreddit: string,
     submissionName: string | null,
