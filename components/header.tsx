c31f8db84e9af328dd34fa5670aad5d70de2f079
cosmos-explorer <lian.yang.work@gmail.com>
2024-05-29 22:31:50 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -32,7 +32,7 @@ function AuthenticatedUserHeader() {
     <div className="w-full border-b p-4 sm:px-6 sm:py-0 md:gap-8">
       <header className="flex items-center justify-between py-2">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6" aria-label="Logo">
+          <Link href="/" className="mr-6 space-x-0.5" aria-label="Logo">
             <span className="font-extrabold">R</span>eddit
             <span className="font-extrabold">L</span>eads
           </Link>
