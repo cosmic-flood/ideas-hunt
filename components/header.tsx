956fad1e3681c8e771b64d742dd468a2d73b9b45
cosmos-explorer <lian.yang.work@gmail.com>
2024-05-29 14:44:28 +0200

    added nave links

--- components/header.tsx
+++ components/header.tsx
@@ -10,12 +10,20 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import Link from 'next/link';
+import NavLinks from '@/components/ui/navlinks';
 
 export default function Header() {
   return (
     <div className="w-full border-b p-4 sm:px-6 sm:py-0 md:gap-8">
       <header className="flex items-center justify-between py-2">
-        <h2 className="text-lg font-semibold">Reddit Sales</h2>
+        <div className="mr-4 flex">
+          <Link href="/" className="mr-6" aria-label="Logo">
+            <span className="font-extrabold">R</span>eddit
+            <span className="font-extrabold">L</span>eads
+          </Link>
+          <NavLinks></NavLinks>
+        </div>
         <User />
       </header>
     </div>
---
+++ components/ui/navlinks.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { cn } from '@/utils/cn';
+
+export default function NavLinks() {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
+  return (
+    <nav className="flex items-center gap-4 text-sm lg:gap-6">
+      <Link
+        href="/"
+        className={cn(
+          currentPath === '/' ? 'font-extrabold' : '',
+          'text-foreground/60 transition-colors hover:text-foreground/80',
+        )}
+      >
+        Home
+      </Link>
+      <Link
+        href="/settings"
+        className={cn(
+          currentPath === '/settings' ? 'font-extrabold' : '',
+          'text-foreground/60 transition-colors hover:text-foreground/80',
+        )}
+      >
+        Settings
+      </Link>
+    </nav>
+  );
+}
